e3f62d0295a9317a0695b1f1e82bd353f5bcfac9
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-16 02:09:23 +0300

    Update and rename index.php to index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+function sahand(){
+  
+var A = document.getElementById("Num1").value;
+var X = document.getElementById("Num2").value;
+var Y = document.getElementById("Anjam").value;
+
+document.write(A+X);
+
+
+
+
+}
