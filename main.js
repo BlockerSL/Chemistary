d9bc4ffd06e2035cc8fe3ee63bde48013ee19d9d
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-17 16:22:48 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function sahand()
+{
+  var Tag = document.getElementById("Tag").value;
+  var bag = { html :  <a href="sahand/index.html" ></a>}
+
+  if (Tag == "H" )
+  {
+    document.write(bag);
+
+  }
+} 
